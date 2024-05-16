2a944167fef2c53e130aeef9802d56a9382d4fe0
vwegba5522 <ogheneochukosalem@gmail.com>
2024-05-16 15:31:53 +0100

    Add

--- Asana-Landing-Page/script.js
+++ Asana-Landing-Page/script.js
@@ -1,20 +1,13 @@
-// JavaScript code to toggle dropdown menu
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
+// // JavaScript code to toggle dropdown menu
+// document.addEventListener("click", function() {
+//     const menuToggle = document.querySelector('.menu-toggle');
+//     const dropdownMenu = document.querySelector('.dropdown-menu');
 
-    menuToggle.addEventListener('click', function() {
-        menuToggle.classList.toggle('open');
-        dropdownMenu.classList.toggle('show');
-    });
+//     menuToggle.addEventListener('click', function() {
+//         menuToggle.classList.toggle('open');
+//         dropdownMenu.classList.toggle('show');
+//     });
 
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.menu-toggle') && !event.target.closest('.dropdown-menu')) {
-            if (dropdownMenu.classList.contains('show')) {
-                dropdownMenu.classList.remove('show');
-                menuToggle.classList.remove('open');
-            }
-        }
-    }
-});
+//     // Close the dropdown menu if the user clicks outside of it
+   
+// });
